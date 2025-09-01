7e8de78c3418bffe23165e30b61aa91696a39cca
Diego Navarrete <diego.navarrete2@mail.udp.cl>
2025-09-01 12:59:54 -0400

    credenciales y más modificaciones

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
 // Inicializar la aplicación Express
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors({
@@ -17,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Conexión MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://diegonavarrete2:Dn8972nV@clusternava.ductuw3.mongodb.net/', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
