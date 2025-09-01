ff473d8e1dbaec5f0bf91558f3d557789e23491b
Diego Navarrete <diego.navarrete2@mail.udp.cl>
2025-09-01 10:56:37 -0400

    cambios variables en backend

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ const PORT = 5000; // Puerto en el que se ejecutará el servidor
 // --- Middlewares ---
 // 1. CORS: Permite solicitudes desde otros orígenes (tu frontend de React)
 app.use(cors({
-  origin: 'http://localhost:8080' // Reemplaza con la URL de tu frontend si es diferente
+  origin: ['https://frontend-autonomia.vercel.app/', 'http://localhost:8080'], // Reemplaza con la URL de tu frontend si es diferente
+  methods: ['GET', 'POST'],
+  credentials : true
 }));
 
 // 2. Body Parser: Permite a Express leer el cuerpo de las solicitudes POST en formato JSON
